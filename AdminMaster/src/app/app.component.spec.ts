edf4f5a4d3dcbf945e77f13228eb24dcef50ebc0
Freyder Diaz <freyderjapo@gmail.com>
2025-04-12 17:25:28 -0500

    Agular 19

--- AdminMaster/src/app/app.component.spec.ts
+++ AdminMaster/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'proyecto_adminmaster' title`, () => {
+  it(`should have the 'AdminMaster' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyecto_adminmaster');
+    expect(app.title).toEqual('AdminMaster');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proyecto_adminmaster');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AdminMaster');
   });
 });
