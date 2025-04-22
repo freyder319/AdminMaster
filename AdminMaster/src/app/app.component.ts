7e8d4236feddb7a3668f9da70da80800b3f9c936
Cristian-Camilo-Aguilar <ca6918950@gmail.com>
2025-04-21 20:53:22 -0500

    Interfaz Cliente 1

--- AdminMaster/src/app/app.component.ts
+++ AdminMaster/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { CarouselComponent } from "./carousel/carousel.component";
+import { ProductsComponent } from "./products/products.component";
+import { LoginAdminComponent } from "./login-admin/login-admin.component";
+import { LoginPuntoVentaComponent } from "./login-punto-venta/login-punto-venta.component";
+import { SecurityEmailComponent } from "./security-email/security-email.component";
+import { SecurityTelefComponent } from './security-telef/security-telef.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, CarouselComponent, ProductsComponent, LoginAdminComponent, LoginPuntoVentaComponent, SecurityEmailComponent, SecurityTelefComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
