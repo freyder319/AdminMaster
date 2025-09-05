caba14851c8fc858d60c85055e58943e4cec09b1
Cristian-Camilo-Aguilar <ca6918950@gmail.com>
2025-09-05 12:27:20 -0500

    Modificacion de Navbar SCRUM 4

--- AdminMaster/src/app/admin_principal/admin_principal.component.ts
+++ AdminMaster/src/app/admin_principal/admin_principal.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AdminNavbarComponent } from '../admin_navbar/admin_navbar.component';
 import { RouterModule } from '@angular/router';
 import { FilterComponent } from '../filter/filter.component';
+
 @Component({
   selector: 'app-administrador-principal',
   imports: [AdminNavbarComponent, RouterModule, FilterComponent],
