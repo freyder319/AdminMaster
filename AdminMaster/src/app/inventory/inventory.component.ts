1631a1bb4100c615e53d8fddbfc51d000032ae60
Cristian-Camilo-Aguilar <ca6918950@gmail.com>
2025-09-11 16:57:37 -0500

    chore: backup before ng update

--- AdminMaster/src/app/inventory/inventory.component.ts
+++ AdminMaster/src/app/inventory/inventory.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AdminNavbarComponent } from "../admin_navbar/admin_navbar.component";
 import { CreateProductoComponent } from "../create_producto/create_producto.component";
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-inventory',
-  imports: [AdminNavbarComponent, CreateProductoComponent],
+  imports: [AdminNavbarComponent, CreateProductoComponent, PaginatorModule],
   templateUrl: './inventory.component.html',
   styleUrls: ['./inventory.component.scss']
 })
