0198ea2555ba23cf710185cb1f59649913a2867b
Freyder Diaz <freyderjapo@gmail.com>
2025-09-16 23:16:51 -0500

    Revert "Revert "Creacion de carpetas de servicios. Creacion de servicios de clientes …""

--- AdminMaster/src/app/clientes/clientes.component.ts
+++ AdminMaster/src/app/clientes/clientes.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { AdminNavbarComponent } from "../admin_navbar/admin_navbar.component";
 import { AddClientesComponent } from "../add_clientes/add_clientes.component";
-import { ModifyClienteComponent } from "../modify-cliente/modify-cliente.component";
+import { Clientes, ClientesService } from '../services/clientes.service';
+import { NgFor } from "@angular/common";
 
 @Component({
   selector: 'app-clientes',
-  standalone: true,
-  imports: [AdminNavbarComponent, AddClientesComponent, ModifyClienteComponent, CommonModule, RouterModule],
+  imports: [AdminNavbarComponent, AddClientesComponent, NgFor],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.scss'
 })
-export class ClientesComponent {
-  mostrarAddCliente = false; 
-  mostrarModificarCliente = false;
+export class ClientesComponent implements OnInit {
+  clientes: Clientes[] = [];
+  constructor(private clientesServices: ClientesService){}
+  ngOnInit(): void {
+      this.clientesServices.getClientes().subscribe({
+        next: (data) => this.clientes = data,
+        error: (error) => console.error('Error al cargar clientes:', error)
+      });
+  }
 }
