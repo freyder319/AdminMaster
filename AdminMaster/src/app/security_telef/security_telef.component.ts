14eafff6d951fb9ad27697d1ad32571f97c26327
Cristian-Camilo-Aguilar <ca6918950@gmail.com>
2025-09-19 17:27:16 -0500

    Configuración Rutas y Solucion Logins

--- AdminMaster/src/app/security_telef/security_telef.component.ts
+++ AdminMaster/src/app/security_telef/security_telef.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-security-telef',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './security_telef.component.html',
   styleUrl: './security_telef.component.scss'
 })
